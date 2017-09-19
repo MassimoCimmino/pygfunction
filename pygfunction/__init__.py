ec0b2b6876d594e1107038a51aabb7c5e34c7729
Unknown <massimo.cimmino@polymtl.ca>
2017-09-19 16:17:18 -0400

    Load load_aggregation in __init__

--- pygfunction/__init__.py
+++ pygfunction/__init__.py
@@ -3,4 +3,5 @@ from __future__ import division, print_function, absolute_import
 from . import boreholes
 from . import gfunction
 from . import heat_transfer
+from . import load_aggregation
 from . import pipes
