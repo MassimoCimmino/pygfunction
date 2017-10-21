4e6c9eac7f481c2cb33b75dddfdc47503c198c8a
Unknown <massimo.cimmino@polymtl.ca>
2017-10-21 12:10:25 -0400

    my first test

---
+++ tests/test_foo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+""" Test for usage of Travis CI.
+"""
+from __future__ import division, print_function, absolute_import
+
+import unittest
+
+class TestUnitTest(unittest.TestCase):
+
+    def test_foo(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
