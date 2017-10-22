70ef9240e61b5c3658bafc32c79b36334b226c94
Unknown <massimo.cimmino@polymtl.ca>
2017-10-22 15:28:56 -0400

    Remove g-function cases for faster testing

--- tests/test_gfunction.py
+++ tests/test_gfunction.py
@@ -27,9 +27,8 @@ class TestUniformHeatExtractionRate(unittest.TestCase):
         from pygfunction.boreholes import rectangle_field
 
         # Results of Cimmino and Bernier (2014)
-        time = np.array([1070, 131118, 33554478])*3600.
+        time = np.array([1070, 33554478])*3600.
         g_ref = np.array([3.65501492640735,
-                          5.87704770495107,
                           6.68675948788837])
 
         # Calculation of the g-function at the same time values
@@ -50,9 +49,8 @@ class TestUniformHeatExtractionRate(unittest.TestCase):
         from pygfunction.boreholes import rectangle_field
 
         # Results of Cimmino and Bernier (2014)
-        time = np.array([1070, 131118, 33554478])*3600.
+        time = np.array([1070, 33554478])*3600.
         g_ref = np.array([3.66173047944222,
-                          11.2627341238639,
                           16.0243474955568])
 
         # Calculation of the g-function at the same time values
@@ -79,16 +77,15 @@ class TestUniformTemperature(unittest.TestCase):
         self.B = 7.5            # Borehole spacing [m]
         self.alpha = 1.0e-6     # Ground thermal diffusivity [m2/s]
 
-    def test_one_borehole_one_segment(self, rel_tol=1.0e-3):
+    def test_one_borehole_one_segment(self, rel_tol=1.0e-4):
         """ Tests the value of the g-function of one borehole.
         """
         from pygfunction.gfunction import uniform_temperature
         from pygfunction.boreholes import rectangle_field
 
         # Results of Cimmino and Bernier (2014)
-        time = np.array([1070, 131118, 33554478])*3600.
+        time = np.array([1070, 33554478])*3600.
         g_ref = np.array([3.65502692098609,
-                          5.87704904232202,
                           6.68675948788828])
 
         # Calculation of the g-function at the same time values
@@ -102,16 +99,15 @@ class TestUniformTemperature(unittest.TestCase):
                         msg='Incorrect values of the g-function of one '
                             'borehole for uniform temperature (1 segment).')
 
-    def test_three_by_two_one_segment(self, rel_tol=1.0e-3):
+    def test_three_by_two_one_segment(self, rel_tol=1.0e-4):
         """ Tests the value of the g-function of a 3 by 2 bore field.
         """
         from pygfunction.gfunction import uniform_temperature
         from pygfunction.boreholes import rectangle_field
 
         # Results of Cimmino and Bernier (2014)
-        time = np.array([1070, 131118, 33554478])*3600.
+        time = np.array([1070, 33554478])*3600.
         g_ref = np.array([3.6617432932388,
-                          11.2153482087534,
                           15.9710298219412])
 
         # Calculation of the g-function at the same time values
@@ -125,16 +121,15 @@ class TestUniformTemperature(unittest.TestCase):
                         msg='Incorrect values of the g-function of three by '
                             'two field for uniform temperature (1 segment).')
 
-    def test_one_borehole_twelve_segments(self, rel_tol=1.0e-3):
+    def test_one_borehole_twelve_segments(self, rel_tol=1.0e-4):
         """ Tests the value of the g-function of one borehole.
         """
         from pygfunction.gfunction import uniform_temperature
         from pygfunction.boreholes import rectangle_field
 
         # Results of Cimmino and Bernier (2014)
-        time = np.array([1070, 131118, 33554478])*3600.
+        time = np.array([1070, 33554478])*3600.
         g_ref = np.array([3.65476902065229,
-                          5.85967970721258,
                           6.6329923757352])
 
         # Calculation of the g-function at the same time values
@@ -148,16 +143,15 @@ class TestUniformTemperature(unittest.TestCase):
                         msg='Incorrect values of the g-function of one '
                             'borehole for uniform temperature (12 segments).')
 
-    def test_three_by_two_twelve_segments(self, rel_tol=1.0e-3):
+    def test_three_by_two_twelve_segments(self, rel_tol=1.0e-4):
         """ Tests the value of the g-function of a 3 by 2 bore field.
         """
         from pygfunction.gfunction import uniform_temperature
         from pygfunction.boreholes import rectangle_field
 
         # Results of Cimmino and Bernier (2014)
-        time = np.array([1070, 131118, 33554478])*3600.
+        time = np.array([1070, 33554478])*3600.
         g_ref = np.array([3.66148345464598,
-                          11.0406206357965,
                           15.1697321426028])
 
         # Calculation of the g-function at the same time values
