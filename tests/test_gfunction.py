ceeb1192d76ae0339f17bdafb4e157d7e3bf5690
Unknown <massimo.cimmino@polymtl.ca>
2017-10-27 11:52:08 -0400

    Test equal_inlet_temperature

--- tests/test_gfunction.py
+++ tests/test_gfunction.py
@@ -166,6 +166,130 @@ class TestUniformTemperature(unittest.TestCase):
                             'two field for uniform temperature (12 segments).')
 
 
+class TestEqualInletTemperature(unittest.TestCase):
+    """ Test cases for calculation of g-functions using equal inlet fluid
+        temperature boundary condition.
+    """
+
+    def setUp(self):
+        self.H = 150.           # Borehole length [m]
+        self.D = 4.             # Borehole buried depth [m]
+        self.r_b = 0.075        # Borehole radius [m]
+        self.B = 7.5            # Borehole spacing [m]
+        self.alpha = 1.0e-6     # Ground thermal diffusivity [m2/s]
+        self.k_s = 2.0          # Ground thermal conductivity [W/m.K]
+        self.m_flow = 0.5       # Fluid mass flow rate (per borehole) [kg/s]
+        self.cp = 4000.         # Fluid specific heat capacity [J/kg.K]
+
+    def test_one_borehole_twenty_four_segments(self, rel_tol=1.0e-4):
+        """ Tests the value of the g-function of one borehole.
+        """
+        from pygfunction.pipes import SingleUTube
+        from pygfunction.gfunction import equal_inlet_temperature
+        from pygfunction.boreholes import rectangle_field
+
+        # Pipe characteristics (will be overwritten by thermal resistances)
+        r_out = 0.01
+        r_in = 0.0075
+        Ds = 0.06
+        pos = [(-Ds, 0.), (Ds, 0.)]
+        k_g = 1.5
+
+        # Results of Cimmino (2015)
+        # Number of line source segments per borehole
+        nSegments = 24
+        # Very large time values to approximate steady-state
+        time = np.array([1.0e19, 1.0e20])
+        # Borehole thermal resistances [m.K/W]
+        Rb = np.array([0.1, 0.3])
+        # Borehole short-circuit resistance [m.K/W]
+        Ra = 0.25
+        g_ref = np.array([6.63770426518980,
+                          6.65263629558422])
+
+        # Calculation of the g-function at the same time values
+        N_1 = 1
+        N_2 = 1
+        boreField = rectangle_field(N_1, N_2, self.B, self.B,
+                                    self.H, self.D, self.r_b)
+        g = np.zeros_like(g_ref)
+        for i in range(len(Rb)):
+            UTubes = [SingleUTube(
+                    pos, r_in, r_out, borehole, self.k_s, k_g, 0., J=0)
+                      for borehole in boreField]
+            # Overwrite borehole resistances
+            Rd_00 = 2*Rb[i]
+            Rd_01 = 2*Ra*Rd_00/(2*Rd_00 - Ra)
+            for UTube in UTubes:
+                UTube._Rd[0,0] = Rd_00
+                UTube._Rd[1,1] = Rd_00
+                UTube._Rd[0,1] = Rd_01
+                UTube._Rd[1,0] = Rd_01
+                UTube._update_model_variables(self.m_flow, self.cp, nSegments)
+            g[i] = equal_inlet_temperature(boreField, UTubes, self.m_flow,
+                                           self.cp, time, self.alpha,
+                                           nSegments=nSegments)[-1]
+
+        self.assertTrue(np.allclose(g, g_ref, rtol=rel_tol, atol=1e-10),
+                        msg='Incorrect values of the g-function of one '
+                            'borehole for equal inlet fluid temperature '
+                            '(24 segments).')
+
+    def test_four_by_four_twenty_four_segments(self, rel_tol=1.0e-4):
+        """ Tests the value of the g-function of 4 by 4 bore field.
+        """
+        from pygfunction.pipes import SingleUTube
+        from pygfunction.gfunction import equal_inlet_temperature
+        from pygfunction.boreholes import rectangle_field
+
+        # Pipe characteristics (will be overwritten by thermal resistances)
+        r_out = 0.01
+        r_in = 0.0075
+        Ds = 0.06
+        pos = [(-Ds, 0.), (Ds, 0.)]
+        k_g = 1.5
+
+        # Results of Cimmino (2015)
+        # Number of line source segments per borehole
+        nSegments = 24
+        # Very large time values to approximate steady-state
+        time = np.array([1.0e19, 1.0e20])
+        # Borehole thermal resistances [m.K/W]
+        Rb = np.array([0.1, 1.0])
+        # Borehole short-circuit resistance [m.K/W]
+        Ra = 0.25
+        g_ref = np.array([26.3083064038505,
+                          28.0185515095651])
+
+        # Calculation of the g-function at the same time values
+        N_1 = 4
+        N_2 = 4
+        boreField = rectangle_field(N_1, N_2, self.B, self.B,
+                                    self.H, self.D, self.r_b)
+        g = np.zeros_like(g_ref)
+        for i in range(len(Rb)):
+            UTubes = [SingleUTube(
+                    pos, r_in, r_out, borehole, self.k_s, k_g, 0., J=0)
+                      for borehole in boreField]
+            # Overwrite borehole resistances
+            Rd_00 = 2*Rb[i]
+            Rd_01 = 2*Ra*Rd_00/(2*Rd_00 - Ra)
+            for UTube in UTubes:
+                UTube._Rd[0,0] = Rd_00
+                UTube._Rd[1,1] = Rd_00
+                UTube._Rd[0,1] = Rd_01
+                UTube._Rd[1,0] = Rd_01
+                UTube._update_model_variables(self.m_flow, self.cp, nSegments)
+            g[i] = equal_inlet_temperature(boreField, UTubes, self.m_flow,
+                                           self.cp, time, self.alpha,
+                                           nSegments=nSegments)[-1]
+
+        self.assertTrue(np.allclose(g, g_ref, rtol=rel_tol, atol=1e-10),
+                        msg='Incorrect values of the g-function of four by '
+                            'four field for equal inlet fluid temperature '
+                            '(24 segments).')
+
+
 class TestThermalResponseFactors(unittest.TestCase):
     """ Test cases for the evaluation of segment to segment thermal response
         factors.
