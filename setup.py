6687ca6c30491369861bfeeb080903b86d65194c
Unknown <massimo.cimmino@polymtl.ca>
2017-12-01 11:25:18 -0500

    setup.py ready for release 1.0.0

--- setup.py
+++ setup.py
@@ -1,39 +1,39 @@
-from setuptools import setup, find_packages
+import os
+from setuptools import setup
 
-with open("README.md", 'r') as f:
-    long_description = f.read()
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name="pygfunction",
-    version="1.0.0.a0.dev",
-    packages=['pygfunction'],
-    scripts=['examples/comparison_load_aggregation.py',
-	         'examples/custom_bore_field.py',
-	         'examples/custom_bore_field_from_file.py',
-	         'examples/equal_inlet_temperature.py',
-	         'examples/fluid_temperature.py',
-	         'examples/load_aggregation.py',
-	         'examples/multiple_independent_Utubes.py',
-	         'examples/multipole_temperature.py',
-	         'examples/regular_bore_field.py',
-	         'examples/uniform_heat_extraction_rate.py',
-	         'examples/uniform_temperature.py',
-	         ],
-
+    version="1.0.0",
+    packages=['pygfunction',
+			  'pygfunction/examples'],
+	include_package_data=True,
     install_requires=['matplotlib>=1.5.3',
 	                  'numpy>=1.11.3',
 					  'scipy>=1.0.0'],
-
-    package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
-    },
 
     # metadata for upload to PyPI
     author="Massimo Cimmino",
     author_email="massimo.cimmino@polymtl.ca",
     description="A g-function calculator for Python",
+    long_description=read('README.md'),
     license="BSD-3-Clause",
     keywords="Geothermal, Ground-source, Ground heat exchangers",
     url="https://github.com/MassimoCimmino/pygfunction",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+		"Topic :: Scientific/Engineering",
+		"Topic :: Utilities",
+    ],
 )
