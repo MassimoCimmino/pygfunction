8319683c11a08290cf478f2df9ac920585b106d9
Unknown <massimo.cimmino@polymtl.ca>
2018-01-17 17:54:15 -0500

    Relocate _path_to_inlet() and _verify_bore_connectivity()

--- pygfunction/pipes.py
+++ pygfunction/pipes.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.constants import pi
 from scipy.special import binom
 
+from .boreholes import _path_to_inlet, _verify_bore_connectivity
+
 
 class _BasePipe(object):
     """
