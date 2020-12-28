019699aa7f9081d9e4710f73927be40e6d6a2847
j_c_cook <jack.cook@okstate.edu>
2020-12-28 14:56:35 -0600

    create an example for a custom borehole. Show 1) checking geometry, and 2) visualizing

---
+++ pygfunction/examples/custom_borehole.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+""" Example of definition of a bore field using custom borehole positions.
+
+"""
+from __future__ import absolute_import, division, print_function
+
+import pygfunction as gt
+
+
+def main():
+    a = 1
+
+
+if __name__ == '__main__':
+    main()
