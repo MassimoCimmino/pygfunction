7bb3950b0d1569cf30fc539b3416c7abf6ec4182
j_c_cook <jack.cook@okstate.edu>
2020-12-30 15:26:50 -0600

    create an example file for common g-function interfacing

---
+++ pygfunction/examples/common_g_function_interface.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+""" An example showing how to make use of the common g-function interface via
+    - dictionary access
+    - listing arguments
+
+"""
+
+
+def main():
+    a = 1
+
+
+if __name__ == '__main__':
+    main()
