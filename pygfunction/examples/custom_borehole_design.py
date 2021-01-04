293a4901863985f72a61ebba5253dbb2238a865b
j_c_cook <jack.cook@okstate.edu>
2021-01-04 09:51:59 -0600

    change the message at the top of the custom_borehole_design example file

--- pygfunction/examples/custom_borehole_design.py
+++ pygfunction/examples/custom_borehole_design.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-""" Example of definition and visualization of a borehole.
+""" Example designing a borehole; outputting a top-view plot of the borehole and the borehole radius.
 
 """
 from __future__ import absolute_import, division, print_function
