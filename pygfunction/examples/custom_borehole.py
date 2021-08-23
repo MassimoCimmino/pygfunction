70b65018b493199d4f025a45e4621cb5683a00b5
j-c-cook <jack.cook@okstate.edu>
2021-08-23 11:55:32 -0500

    Add coaxial function call

--- pygfunction/examples/custom_borehole.py
+++ pygfunction/examples/custom_borehole.py
@@ -39,10 +39,7 @@ def main():
 
     # Fluid properties
     m_flow_borehole = 0.25  # Total fluid mass flow rate per borehole (kg/s)
-    cp_f = 3977.            # Fluid specific isobaric heat capacity (J/kg.K)
-    rho_f = 1015.           # Fluid density (kg/m3)
-    mu_f = 0.00203          # Fluid dynamic viscosity (kg/m.s)
-    k_f = 0.492             # Fluid thermal conductivity (W/m.K)
+    fluid = gt.media.Fluid(mixer='MEG', percent=0.)
 
     # Pipe thermal resistance
     R_p = gt.pipes.conduction_thermal_resistance_circular_pipe(
@@ -50,28 +47,64 @@ def main():
     # Fluid to inner pipe wall thermal resistance (Single U-tube)
     m_flow_pipe = m_flow_borehole
     h_f = gt.pipes.convective_heat_transfer_coefficient_circular_pipe(
-        m_flow_pipe, r_in, mu_f, rho_f, k_f, cp_f, epsilon)
+        m_flow_pipe, r_in, fluid.mu, fluid.rho, fluid.k, fluid.cp, epsilon)
     R_f = 1.0 / (h_f * 2 * pi * r_in)
 
     SingleUTube = gt.pipes.SingleUTube(
         pos_single, r_in, r_out, borehole, k_s, k_g, R_f + R_p)
 
     R_b = gt.pipes.borehole_thermal_resistance(
-        SingleUTube, m_flow_borehole, cp_f)
+        SingleUTube, m_flow_borehole, fluid.cp)
 
     print('Borehole thermal resistance: {0:.4f} m.K/W'.format(R_b))
 
     # Coaxial pipe
     # Note: Is a coaxial pipe typically set as far as the spacing between the
     #       inner and outer tubes go?
-    pos = (0., 0.)  # Coordinates of the coaxial pipe axis
+    pos = [(0., 0.), (0., 0.)]  # Coordinates of the coaxial pipe axis
     # Pipe dimensions
-    r_inner = np.array([44.2 / 1000. / 2., 50. / 1000. / 2.])  # Inner pipe radii (m)
-    r_outer = np.array([97.4 / 1000. / 2., 110. / 1000. / 2.])  # Outer pip radii (m)
-    epsilon = [1.0e-6, 1.0e-6]  # Pipe roughness (m)
+    r_in_in = 44.2 / 1000. / 2.  # inside pipe inner radius (m)
+    r_in_out = 50. / 1000. / 2.  # inside pipe outer radius (m)
+    r_out_in = 97.4 / 1000. / 2.  # outer pipe inside radius (m)
+    r_out_out = 110. / 1000. / 2.  # outer pipe outside radius (m)
+    r_inner = np.array([r_in_in, r_in_out])  # Inner pipe radii (m)
+    r_outer = np.array([r_out_in, r_out_out])  # Outer pip radii (m)
+    epsilon = 1.0e-6  # Pipe roughness (m)
 
     # Thermal properties
     k_p = [0.4, 0.4]  # Inner and outer pipe thermal conductivity (W/m.K)
+
+    # Fluid-to-fluid thermal resistance
+    # inner fluid thermal resistance
+    h_fluid_in = gt.pipes.convective_heat_transfer_coefficient_circular_pipe(
+        m_flow_borehole, r_in_in, fluid.mu, fluid.rho, fluid.k, fluid.cp,
+        epsilon)
+    R_conv_1 = 1.0 / (h_fluid_in * 2 * pi * r_in_in)
+    # inner pipe thermal resistance
+    R_cyl_1 = gt.pipes.conduction_thermal_resistance_circular_pipe(
+        r_in_in, r_in_out, k_p[0])
+    # inner annulus convection resistance
+    h_fluid_a_in, h_fluid_a_out, Re = \
+        gt.pipes.convective_heat_transfer_coefficient_concentric_annulus(
+            m_flow_borehole, r_in_out, r_out_in, fluid.mu, fluid.rho, fluid.k,
+            fluid.cp, epsilon)
+    R_conv_2 = 1.0 / (h_fluid_a_in * 2 * pi * r_in_out)
+    R_ff = R_conv_1 + R_cyl_1 + R_conv_2
+
+    # Fluid-to-pipe thermal resistance
+    # outer annulus convection resistance
+    R_conv_3 = 1.0 / (h_fluid_a_out * 2 * pi * r_out_in)
+    # outer pipe thermal resistance
+    R_cyl_2 = gt.pipes.conduction_thermal_resistance_circular_pipe(
+        r_out_in, r_out_out, k_p[1])
+    R_fp = R_conv_3 + R_cyl_2
+
+    Coaxial = gt.pipes.Coaxial(
+        pos, r_inner, r_outer, borehole, k_s, k_g, R_ff, R_fp, J=2)
+
+    Rb = gt.pipes.borehole_thermal_resistance(
+        Coaxial, m_flow_borehole, fluid.cp)
+    print(Rb)
 
     # Check the geometry to make sure it is physically possible
     #
