2f6f7019006aeb7458b68f042e958157c19014e0
Massimo Cimmino <massimo.cimmino@polymtl.ca>
2021-09-16 12:05:52 -0400

    Add example to compare solvers

---
+++ pygfunction/examples/comparison_gfunction_solvers.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+""" Comparison of solvers for the evaluation of g-functions using uniform and
+    equal borehole wall temperatures.
+
+    The g-function of a field of 6x4 boreholes is calculated for a boundary
+    condition of uniform borehole wall temperature along the boreholes, equal
+    for all boreholes. Three different solvers are compared : 'detailed',
+    'similarities' and 'equivalent'. Their accuracy and calculation time are
+    compared using the 'detailed' solver as a reference. This shows that the
+    'similarities' solver can evaluate g-functions with high accuracy.
+
+    The g-function of a field of 12x10 boreholes is calculated for a boundary
+    condition of uniform borehole wall temperature along the boreholes, equal
+    for all boreholes. Two different solvers are compared : 'similarities' and
+    'equivalent'. The accuracy and calculation time of the 'equivalent' is
+    compared using the 'similarities' solver as a reference. This shows that
+    the 'equivalent' solver evaluates g-functions at a very high calculation
+    speed while maintaining reasonable accuracy.
+
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from time import time as tic
+
+import pygfunction as gt
+
+
+def main():
+    # -------------------------------------------------------------------------
+    # Simulation parameters
+    # -------------------------------------------------------------------------
+
+    # Borehole dimensions
+    D = 4.0             # Borehole buried depth (m)
+    H = 150.0           # Borehole length (m)
+    r_b = 0.075         # Borehole radius (m)
+    B = 7.5             # Borehole spacing (m)
+
+    # Thermal properties
+    alpha = 1.0e-6      # Ground thermal diffusivity (m2/s)
+
+    # g-Function calculation options
+    options = {'nSegments':12, 'disp':True}
+
+    # Geometrically expanding time vector.
+    dt = 100*3600.                  # Time step
+    tmax = 3000. * 8760. * 3600.    # Maximum time
+    Nt = 15                         # Number of time steps
+    ts = H**2/(9.*alpha)            # Bore field characteristic time
+    time = gt.utilities.time_geometric(dt, tmax, Nt)
+    lntts = np.log(time/ts)
+
+    # -------------------------------------------------------------------------
+    # Borehole field (First bore field)
+    # -------------------------------------------------------------------------
+
+    # Field of 6x4 (n=24) boreholes
+    N_1 = 6
+    N_2 = 4
+    field = gt.boreholes.rectangle_field(N_1, N_2, B, B, H, D, r_b)
+
+    # -------------------------------------------------------------------------
+    # Evaluate g-functions
+    # -------------------------------------------------------------------------
+    t0 = tic()
+    gfunc_detailed = gt.gfunction.gFunction(
+        field, alpha, time=time, options=options, method='detailed')
+    t1 = tic()
+    t_detailed = t1 - t0
+    gfunc_similarities = gt.gfunction.gFunction(
+        field, alpha, time=time, options=options, method='similarities')
+    t2 = tic()
+    t_similarities = t2 - t1
+    gfunc_equivalent = gt.gfunction.gFunction(
+        field, alpha, time=time, options=options, method='equivalent')
+    t3 = tic()
+    t_equivalent = t3 - t2
+
+
+    # -------------------------------------------------------------------------
+    # Plot results
+    # -------------------------------------------------------------------------
+    # Draw g-functions
+    ax = gfunc_detailed.visualize_g_function().axes[0]
+    ax.plot(lntts, gfunc_similarities.gFunc, 'bx')
+    ax.plot(lntts, gfunc_equivalent.gFunc, 'ro')
+    ax.legend(['detailed (t = {:.3f} sec)'.format(t_detailed),
+               'similarities (t = {:.3f} sec)'.format(t_similarities),
+               'equivalent (t = {:.3f} sec)'.format(t_equivalent)])
+    ax.set_title('Field of {} by {} boreholes'.format(N_1, N_2))
+    plt.tight_layout()
+
+    # Draw absolute error
+    # Configure figure and axes
+    fig = gt.utilities._initialize_figure()
+    ax = fig.add_subplot(111)
+    # Axis labels
+    ax.set_xlabel(r'ln$(t/t_s)$')
+    ax.set_ylabel(r'Absolute error')
+    gt.utilities._format_axes(ax)
+    # Absolute error
+    ax.plot(lntts, np.abs(gfunc_similarities.gFunc - gfunc_detailed.gFunc),
+            '-', label='similarities')
+    ax.plot(lntts, np.abs(gfunc_equivalent.gFunc - gfunc_detailed.gFunc),
+            '--', label='equivalent')
+    ax.legend()
+    ax.set_title("Absolute error relative to the 'detailed' solver (Field of {} by {} boreholes)".format(N_1, N_2))
+    # Adjust to plot window
+    plt.tight_layout()
+
+    # Draw relative error
+    # Configure figure and axes
+    fig = gt.utilities._initialize_figure()
+    ax = fig.add_subplot(111)
+    # Axis labels
+    ax.set_xlabel(r'ln$(t/t_s)$')
+    ax.set_ylabel(r'Relative error')
+    gt.utilities._format_axes(ax)
+    # Relative error
+    ax.plot(lntts, (gfunc_similarities.gFunc - gfunc_detailed.gFunc) / gfunc_detailed.gFunc,
+            '-', label='similarities')
+    ax.plot(lntts, (gfunc_equivalent.gFunc - gfunc_detailed.gFunc) / gfunc_detailed.gFunc,
+            '--', label='equivalent')
+    ax.legend()
+    ax.set_title("Relative error relative to the 'detailed' solver (Field of {} by {} boreholes)".format(N_1, N_2))
+    # Adjust to plot window
+    plt.tight_layout()
+
+    # -------------------------------------------------------------------------
+    # Borehole field (Second bore field)
+    # -------------------------------------------------------------------------
+
+    # Field of 6x4 (n=24) boreholes
+    N_1 = 12
+    N_2 = 10
+    field = gt.boreholes.rectangle_field(N_1, N_2, B, B, H, D, r_b)
+
+    # -------------------------------------------------------------------------
+    # Evaluate g-functions
+    # -------------------------------------------------------------------------
+    gfunc_similarities = gt.gfunction.gFunction(
+        field, alpha, time=time, options=options, method='similarities')
+    t2 = tic()
+    t_similarities = t2 - t1
+    gfunc_equivalent = gt.gfunction.gFunction(
+        field, alpha, time=time, options=options, method='equivalent')
+    t3 = tic()
+    t_equivalent = t3 - t2
+
+
+    # -------------------------------------------------------------------------
+    # Plot results
+    # -------------------------------------------------------------------------
+    # Draw g-functions
+    ax = gfunc_similarities.visualize_g_function().axes[0]
+    ax.plot(lntts, gfunc_equivalent.gFunc, 'ro')
+    ax.legend(['similarities (t = {:.3f} sec)'.format(t_similarities),
+               'equivalent (t = {:.3f} sec)'.format(t_equivalent)])
+    ax.set_title('Field of {} by {} boreholes'.format(N_1, N_2))
+    plt.tight_layout()
+
+    # Draw absolute error
+    # Configure figure and axes
+    fig = gt.utilities._initialize_figure()
+    ax = fig.add_subplot(111)
+    # Axis labels
+    ax.set_xlabel(r'ln$(t/t_s)$')
+    ax.set_ylabel(r'Absolute error')
+    gt.utilities._format_axes(ax)
+    # Absolute error
+    ax.plot(lntts, np.abs(gfunc_equivalent.gFunc - gfunc_similarities.gFunc),
+            label='equivalent')
+    ax.legend()
+    ax.set_title("Absolute error relative to the 'similarities' solver (Field of {} by {} boreholes)".format(N_1, N_2))
+    # Adjust to plot window
+    plt.tight_layout()
+
+    # Draw relative error
+    # Configure figure and axes
+    fig = gt.utilities._initialize_figure()
+    ax = fig.add_subplot(111)
+    # Axis labels
+    ax.set_xlabel(r'ln$(t/t_s)$')
+    ax.set_ylabel(r'Relative error')
+    gt.utilities._format_axes(ax)
+    # Relative error
+    ax.plot(lntts, (gfunc_equivalent.gFunc - gfunc_similarities.gFunc) / gfunc_similarities.gFunc,
+            label='equivalent')
+    ax.legend()
+    ax.set_title("Relative error relative to the 'similarities' solver (Field of {} by {} boreholes)".format(N_1, N_2))
+    # Adjust to plot window
+    plt.tight_layout()
+
+    return
+
+
+# Main function
+if __name__ == '__main__':
+    main()
