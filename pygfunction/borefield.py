5f8652001607216dc5a0b5d8f6fe3e7dd1485bec
MassimoCimmino <massimo.cimmino@polymtl.ca>
2024-11-27 09:59:48 -0500

    Change init to use arrays instead of boreholes

--- pygfunction/borefield.py
+++ pygfunction/borefield.py
@@ -9,30 +9,36 @@ from .boreholes import Borehole
 class Borefield:
     """This class represents the borehole field and can generate inputs for pygfunction"""
 
-    def __init__(self, boreholes: Union[Borehole, List[Borehole]]):
-        if isinstance(boreholes, Borehole):
-            boreholes = [boreholes]
-        self.boreholes = boreholes
-        self.nBoreholes = len(boreholes)
-        self.H = np.array([b.H for b in boreholes])
-        self.D = np.array([b.D for b in boreholes])
-        self.r_b = np.array([b.r_b for b in boreholes])
-        self.tilt = np.array([b.tilt for b in boreholes])
-        self.orientation = np.array([b.orientation for b in boreholes])
-        self.x = np.array([b.x for b in boreholes])
-        self.y = np.array([b.y for b in boreholes])
-        self._is_tilted = np.array(
-            [b._is_tilted for b in boreholes], dtype=bool)
+    def __init__(
+            self, H: npt.ArrayLike, D: npt.ArrayLike, r_b: npt.ArrayLike,
+            x: npt.ArrayLike, y: npt.ArrayLike, tilt: npt.ArrayLike = 0,
+            orientation: npt.ArrayLike = 0):
+        # Convert x and y coordinates to arrays
+        x = np.atleast_1d(x)
+        y = np.atleast_1d(y)
+        self.nBoreholes = np.maximum(len(x), len(y))
+
+        # Broadcast all variables to arrays of length `nBoreholes`
+        self.H = np.broadcast_to(H, self.nBoreholes)
+        self.D = np.broadcast_to(D, self.nBoreholes)
+        self.r_b = np.broadcast_to(r_b, self.nBoreholes)
+        self.x = np.broadcast_to(x, self.nBoreholes)
+        self.y = np.broadcast_to(y, self.nBoreholes)
+        self.tilt = np.broadcast_to(tilt, self.nBoreholes)
+        self.orientation = np.broadcast_to(orientation, self.nBoreholes)
+
+        # Identify tilted boreholes
+        self._is_tilted = np.greater(self.orientation, 1e-6)
         pass
 
     def __getitem__(self, key):
-        if isinstance(key, slice):
-            return Borefield(self.boreholes[key])
-        elif isinstance(key, int):
-            return self.boreholes[key]
-        elif isinstance(key, (list, np.ndarray)):
-            return Borefield([self.boreholes[i] for i in key])
-        return
+        if isinstance(key, (int, np.integer)):
+            output_class = Borehole
+        else:
+            output_class = Borefield
+        return output_class(
+            self.H[key], self.D[key], self.r_b[key], self.x[key], self.y[key],
+            tilt=self.tilt[key], orientation=self.orientation[key])
 
     def __len__(self):
          return self.nBoreholes
@@ -62,40 +68,18 @@ class Borefield:
         return borefield
 
     @classmethod
-    def from_lists(
-            cls, H: npt.ArrayLike, D: npt.ArrayLike, r_b: npt.ArrayLike,
-            x: npt.ArrayLike, y: npt.ArrayLike, tilt: npt.ArrayLike = 0,
-            orientation: npt.ArrayLike = 0) -> Self:
-        # Convert x and y coordinates to arrays
-        x = np.atleast_1d(x)
-        y = np.atleast_1d(y)
-        nBoreholes = np.maximum(len(x), len(y))
-
-        def to_array(var):
-            """Converts `var` to array of length `nBoreholes`"""
-            if np.isscalar(var) or (
-                    isinstance(var, (np.ndarray, list)) and len(var)==1):
-                var = np.full(nBoreholes, var)
-            assert len(var) == nBoreholes, \
-                f"Expected list of length {nBoreholes}, but got {len(var)}."
-            return var
-
-        # Convert all variables to arrays of length `nBoreholes`
-        H = to_array(H)
-        D = to_array(D)
-        r_b = to_array(r_b)
-        x = to_array(x)
-        y = to_array(y)
-        tilt = to_array(tilt)
-        orientation = to_array(orientation)
-
-        # Create list of boreholes
-        boreholes = [
-            Borehole(
-                _H, _D, _r_b, _x, _y, tilt=_tilt, orientation=_orientation)
-            for _H, _D, _r_b, _x, _y, _tilt, _orientation in zip(
-                    H, D, r_b, x, y, tilt, orientation)]
-        borefield = cls(boreholes)
+    def from_boreholes(
+            cls, boreholes: Union[Borehole, List[Borehole]]) -> Self:
+        if isinstance(boreholes, Borehole):
+            boreholes = [boreholes]
+        H = np.array([b.H for b in boreholes])
+        D = np.array([b.D for b in boreholes])
+        r_b = np.array([b.r_b for b in boreholes])
+        tilt = np.array([b.tilt for b in boreholes])
+        orientation = np.array([b.orientation for b in boreholes])
+        x = np.array([b.x for b in boreholes])
+        y = np.array([b.y for b in boreholes])
+        borefield = cls(H, D, r_b, x, y, tilt=tilt, orientation=orientation)
         return borefield
 
     def evaluate_g_function(
