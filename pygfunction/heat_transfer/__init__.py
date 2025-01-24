02c72c471d83f8e781bb57dfd7f9e852c986f8ae
MassimoCimmino <massimo.cimmino@polymtl.ca>
2025-01-24 04:52:24 -0500

    Split heat_transfer into multiple files

---
+++ pygfunction/heat_transfer/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from .finite_line_source import finite_line_source, finite_line_source_equivalent_boreholes_vectorized
+from .finite_line_source_vertical import finite_line_source_vertical
+from .finite_line_source_inclined import finite_line_source_inclined
