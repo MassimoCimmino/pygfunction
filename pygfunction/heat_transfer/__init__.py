2e13a5a71bc2c5118f43b8a26696eca3a6b7c4bc
MassimoCimmino <massimo.cimmino@polymtl.ca>
2025-04-29 05:38:55 -0400

    Update doc

--- pygfunction/heat_transfer/__init__.py
+++ pygfunction/heat_transfer/__init__.py
@@ -1,3 +1,9 @@
 from .finite_line_source import finite_line_source
 from .finite_line_source_vertical import finite_line_source_vertical
 from .finite_line_source_inclined import finite_line_source_inclined
+
+__all__ = [
+'finite_line_source',
+'finite_line_source_vertical',
+'finite_line_source_inclined'
+]
